d54ae90a92cdfd8160b3086a6d6de4846c31b848
Bas Sondag <bassondag@Bass-MacBook-Pro.local>
2019-11-10 10:36:18 -0800

    UPDATE: api/people.rb  added route to find user by name

--- api/people.rb
+++ api/people.rb
@@ -7,6 +7,13 @@ module Base
         people = DB[:users]
         people.all
       end
+      desc "Return a person"
+      params do
+        requires :id, type: String, desc: "Name of the person"
+      end
+      get ":id", root: "person" do
+        DB[:users].where(name: params[:id]).first!
+      end
     end
   end
 end
